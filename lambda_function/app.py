8df8265311e702d65bd52ed37800e7d81d3048a0
madhurashree <madhurashree@madhura.local>
2025-11-08 13:48:12 +0530

    Update lambda code

--- lambda_function/app.py
+++ lambda_function/app.py
@@ -4,6 +4,6 @@ def handler(event, context):
     name = event.get("name", "World")
     return {
         'statusCode': 200,
-        'body': f"Hello {name}!, Welcome to devops serverless!"
+        'body': f"Hello {name}!, Welcome to devops serverless!, Now deployed form github actions"
     }
     
