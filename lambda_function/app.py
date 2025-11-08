09162c88dd78d6bba76410410ab8b89c9f044ce3
madhurashree <madhurashree@madhura.local>
2025-11-08 13:35:16 +0530

    Serverless CICD using github actions

---
+++ lambda_function/app.py
@@ -0,0 +1,9 @@
+import json
+
+def lambda_handler(event, context):
+    name = event.get("name", "World")
+    return {
+        'statusCode': 200,
+        'body': f"Hello {name}!, Welcome to devops serverless!"
+    }
+    
